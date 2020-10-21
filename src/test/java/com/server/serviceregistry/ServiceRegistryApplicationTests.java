81d824877d8309bae3f252605642d1c0c731ec95
Eureka server <kvineet881@gmail.com>
2020-10-21 19:22:23 +0530

    Eureka Server is Created

---
+++ src/test/java/com/server/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
